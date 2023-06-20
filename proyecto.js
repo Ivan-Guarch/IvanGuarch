0772db0a870552a734196167fab240f0cb5b2d51
Iván Guarch <ivan.guarch1@gmail.com>
2023-06-19 22:06:12 -0300

    commit inicial

---
+++ proyecto.js
@@ -0,0 +1,10 @@
+/*
+let numero0 = parseInt(prompt("Dime un Numero entre 50 y 60"))
+
+if ((numero0>=50) && (numero0<=60)){
+    alert("Tu numero cumple los requisitos")
+}
+else {
+    alert("Tu numero no cumple con los requisitos")
+}
+*/
